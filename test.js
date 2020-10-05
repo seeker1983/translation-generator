a51fa2a582f8003b066aeebe0c2ab1b3e7a34229
Roman K (seeker1983) <krupod@gmail.com>
2020-10-05 15:34:33 +0300

    test fix

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 const config = require('./config')
-const s1 = fs.readFileSync('test/translation-structure-example.json', config.ENCODING);
-const s2 = fs.readFileSync(config.STRUCTURE_FILE, config.ENCODING);
+const s1 = fs.readFileSync(__dirname + '/' + 'test/translation-structure-example.json', config.ENCODING);
+const s2 = fs.readFileSync(__dirname + '/' + config.STRUCTURE_FILE, config.ENCODING);
 
 if(s1 == s2)
 	console.log('Structure matches.');
 else
 	throw 'Structure mismatch!';
 
-const s3 = fs.readFileSync('test/default-example.po', config.ENCODING);
-const s4 = fs.readFileSync(config.OUTPUT_FILE, config.ENCODING);
+const s3 = fs.readFileSync(__dirname + '/' + 'test/default-example.po', config.ENCODING);
+const s4 = fs.readFileSync(__dirname + '/' + config.OUTPUT_FILE, config.ENCODING);
 
 if(s3 == s4)
 	console.log('Glossary matches.');
